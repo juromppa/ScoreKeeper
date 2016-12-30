ebb520ede87ed1cbf7e1fa0d1dc48d7049d288ea
Juha Romppainen <juha.romppainen@bees.fi>
2016-12-30 23:42:54 +0200

    menu toimii

---
+++ app/src/main/java/com/gmail/romppainen/matti/juha/scorekeeper/MainActivity.java
@@ -0,0 +1,76 @@
+package com.gmail.romppainen.matti.juha.scorekeeper;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), MainActivity
+                .this);
+        viewPager.setAdapter(pagerAdapter);
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    class PagerAdapter extends FragmentPagerAdapter {
+        String tabTitles[] = getResources().getStringArray(R.array.main_tab_titles);
+        Context context;
+
+        PagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return tabTitles.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new MainActivityTabFragment1();
+                case 1:
+                    return new MainActivityTabFragment2();
+                case 2:
+                    return new MainActivityTabFragment3();
+                case 3:
+                    return new MainActivityTabFragment4();
+            }
+
+            return null;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            // Generate title based on item position
+            return tabTitles[position];
+        }
+    }
+}
---
+++ app/src/main/java/com/gmail/romppainen/matti/juha/scorekeeper/MainActivityTabFragment4.java
@@ -0,0 +1,89 @@
+package com.gmail.romppainen.matti.juha.scorekeeper;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Juha on 28.12.2016.
+ */
+
+public class MainActivityTabFragment4 extends Fragment {
+
+    String[] values = new String[]{"Android", "iPhone", "WindowsMobile",
+            "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+            "Linux", "OS/2", "iPhone", "WindowsMobile",
+            "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X"};
+
+    public MainActivityTabFragment4() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
+            savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.main_activity_tab_fragment_4, container, false);
+
+        RecyclerView rv = (RecyclerView) rootView.findViewById(R.id.rv_main_fragment_4);
+        rv.setHasFixedSize(true);
+        MyAdapter adapter = new MyAdapter(values);
+        rv.setAdapter(adapter);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(llm);
+
+        return rootView;
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+        private String[] mDataset;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            TextView mTextView;
+
+            MyViewHolder(View v) {
+                super(v);
+
+                mTextView = (TextView) v.findViewById(R.id.label);
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        MyAdapter(String[] myDataset) {
+            mDataset = myDataset;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            // create a new view
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.players_item,
+                    parent, false);
+            // set the view's size, margins, paddings and layout parameters
+            return new MyViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.mTextView.setText(mDataset[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
+    }
+}
